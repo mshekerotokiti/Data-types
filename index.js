25f76d04bc334b0d68a6e4e2c5778ff8a09b4a0b
mshekerotokiti <sharontokiti@gmail.com>
2023-01-05 09:49:41 +0300

    my first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let isIsland = true;
+isIsland = "lamu";
+console. log(isIsland);
+
+let language = true;
+language ="maasai";
+console.log(language);
+
+let country = true;
+country= "Tanzania";
+console.log(country);
+
